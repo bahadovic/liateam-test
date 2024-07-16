4d3a28e62fccbe21c2feb9a3799eef9d89037efe
bahadovic <b.hosseini@arzeshwallet.com>
2024-07-16 21:02:01 +0330

    fix User.php model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    protected $connection = 'mongodb';
 
     protected $fillable = ['name', 'email', 'password'];
 
