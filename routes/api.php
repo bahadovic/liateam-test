61aec1b0ff4aaaf63113d14aca867fe749b1ab2b
bahadovic <b.hosseini@arzeshwallet.com>
2024-07-16 19:23:07 +0330

    add api rout

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,18 @@
 <?php
 
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->name('auth.')->controller(AuthController::class)->group(function () {
     Route::post('login', 'login')->middleware('guest')->name('login');
     Route::get('refresh-token', 'refreshToken')->middleware('guest')->name('refresh-token');
     Route::post('logout', 'logout')->middleware('auth')->name('logout');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::apiResource('products', ProductController::class);
+    Route::apiResource('orders', OrderController::class);
 });
